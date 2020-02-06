a0c6892cc6e8f1c35cfc1269fa42250893733275
camChouette <benhmida.camelia@gmail.com>
2020-02-06 21:09:16 +0100

    ajout

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,20 @@ let canvas : any = document.getElementById( "canvas" );
 let engine = new BABYLON.Engine( canvas, true );
 let scene = new BABYLON.Scene( engine );
 
+var skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
+
+//skybox.infiniteDistance = true; // <-- with this you can make the skybox of infinite size
+	    
+// create material for the skybox
+var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+skyboxMaterial.backFaceCulling = false;
+skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("images/seabox/sea", scene);
+skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+
+skybox.material = skyboxMaterial;
+			
 //
 // Camera
 //
