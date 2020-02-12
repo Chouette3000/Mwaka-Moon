66ff328dc300ecf709bfb16a96b552c2ac397193
camChouette <benhmida.camelia@gmail.com>
2020-02-12 19:13:21 +0100

    #15 rebond sur la plateforme

--- src/obj/plateforme.js
+++ src/obj/plateforme.js
@@ -9,7 +9,7 @@ var getNewPlateforme = function (scene, playerCamera) {
 	var boxPlateforme = BABYLON.MeshBuilder.CreateBox("box", { height: 5, width: 150.00, depth: 150.00}, scene);
 	boxPlateforme.position.y = 150;
 	boxPlateforme.material = mat;
-	boxPlateforme.physicsImpostor = new BABYLON.PhysicsImpostor(boxPlateforme, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0, restitution: 0.1 }, scene);
+	boxPlateforme.physicsImpostor = new BABYLON.PhysicsImpostor(boxPlateforme, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0, restitution: 0.9 }, scene);
     boxPlateforme.convertToFlatShadedMesh();
 	playerCamera.lockedTarget = boxPlateforme;
 	//console.log(boxPlateforme.position);
