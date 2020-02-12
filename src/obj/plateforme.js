3ca71f60eb91c72734f4c70e3d999e439d73c30a
camChouette <benhmida.camelia@gmail.com>
2020-02-12 19:03:18 +0100

    #15 plateforme in separate obj

--- src/obj/plateforme.js
+++ src/obj/plateforme.js
@@ -3,15 +3,16 @@ var getNewPlateforme = function (scene, playerCamera) {
 	
 	var mat = new BABYLON.StandardMaterial("mat", scene);
 	mat.ambientTexture = new BABYLON.Texture("resources/textures/distortion.png", scene);
-	mat.alpha = 0.6;
+	mat.alpha = 0.7;
 
 	// Plateforme
-	var boxPlateforme = BABYLON.MeshBuilder.CreateBox("box", { height: 5, width: 45.00, depth: 45.00}, scene);
-	boxPlateforme.position.y = 30;
+	var boxPlateforme = BABYLON.MeshBuilder.CreateBox("box", { height: 5, width: 150.00, depth: 150.00}, scene);
+	boxPlateforme.position.y = 150;
 	boxPlateforme.material = mat;
 	boxPlateforme.physicsImpostor = new BABYLON.PhysicsImpostor(boxPlateforme, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0, restitution: 0.1 }, scene);
     boxPlateforme.convertToFlatShadedMesh();
 	playerCamera.lockedTarget = boxPlateforme;
+	//console.log(boxPlateforme.position);
 	return boxPlateforme;
 }
 
