c8e998127db2396bb28c293f45128aba786e4d5f
Lion3000 <alex.zarzitski@gmail.com>
2020-02-12 18:38:53 +0100

    maj sky box #23

--- src/obj/univers.js
+++ src/obj/univers.js
@@ -15,7 +15,7 @@ class Univers {
   }
 
   initSkybox(){
-    this.skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, this.scene);
+    this.skybox = BABYLON.Mesh.CreateBox("skyBox", 15000.0, this.scene);
     var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", this.scene);
     skyboxMaterial.backFaceCulling = false;
     skyboxMaterial.disableLighting = true;
@@ -28,12 +28,12 @@ class Univers {
   }
 
   async initGround(){
-    this.ground = await BABYLON.Mesh.CreateGroundFromHeightMapAsync("ground", "resources/textures/moon_height_map.png", 1000, 1000, 50, 0, 100, this.scene, false);
+    this.ground = await BABYLON.Mesh.CreateGroundFromHeightMapAsync("ground", "resources/textures/moon.jpg", 1000, 1000, 100, 0, 100, this.scene, false);
     this.ground.physicsImpostor = new BABYLON.PhysicsImpostor(this.ground, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0, restitution: 0.1 }, this.scene);
     this.ground.convertToFlatShadedMesh();
 
     var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
-    groundMaterial.diffuseTexture = new BABYLON.Texture("resources/textures/moon.jpeg", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("resources/textures/moon.jpg", scene);
     groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
     this.ground.material = groundMaterial;
 
